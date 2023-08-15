0f80feb13b7fbb2c6295665cb17177828c5d8491
Serhii Khrychov <serhii.khrychov@orbussoftware.com>
2023-08-15 21:49:09 +0300

    turn off webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,10 +38,10 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
